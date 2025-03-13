423252caf6687a93a3493093acaba4c70d986af6
tatturun <231205041@ccmailg.meijo-u.ac.jp>
2025-03-14 08:05:45 +0900

    [update] #14 aiを改良

--- lib/ui_chat.dart
+++ lib/ui_chat.dart
@@ -39,6 +39,8 @@ class _ChatPageState extends State<ChatPage> {
     _model = GenerativeModel(model: 'gemini-2.0-flash', apiKey: apiKey);
     AI = _model.startChat();
     AI.sendMessage(Content.text('これから送る問題を教えて欲しいのですが、解き方を一気に教えられても難しいので順序立てて出力し、こちらの解答を待ってから次にやることを出力するようにしてください'));
+    AI.sendMessage(Content.text('こちらが答えるとき，文章で説明し回答しなければならないような質問を，ときどきお願いします'));
+    AI.sendMessage(Content.text('出力は数式表現やコードフィールドなどの環境依存のものは無しでプレーンテキストでお願いします'));
     AI.sendMessage(Content.text('口調は友達のような感じで大丈夫だよ！'));
   }
 
