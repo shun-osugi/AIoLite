a717d8f9a8bf6e7cfd4dda703ad369f14abdf3e6
shun <shun01.oh@gmail.com>
2025-04-20 23:28:23 +0900

    [add] #94 APIKEYの導入(コメントアウト中)

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,12 @@ import 'subject_categories.dart';
 import 'api_service.dart';
 import 'terms_content.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 
-void main() {
+
+Future<void> main() async{
+  await dotenv.load(); // 環境変数の読み込み
   runApp(const MyApp());
 }
 
