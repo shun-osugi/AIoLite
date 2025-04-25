71d9aefdef50d5f5058714e734d28285854f6f48
Akira Okada <231205043@ccmailg.meijo-u.ac.jp>
2025-04-26 00:16:57 +0900

    [update] #104 ホーム画面のUI調整

--- lib/colors.dart
+++ lib/colors.dart
@@ -4,9 +4,9 @@ class AppColors {
   static const Color mainColor = Color(0xff554488);
   static const Color subColor = Color(0xff5eddff);
   static const Color accentColor = Color(0xfffbff82);
-  static const Color background = Color(0xFFc5d0d6); // ダイアログ等背景
+  static const Color buttonColor = Color(0xff68660e); // ダイアログ等背景
   static const Color background_a = Color(0xFFa59fce); // advanced背景
   static const Color background_b = Color(0xFFfffde1); // basic背景
   static const Color white = Color(0xFFeaeff8); // 白色
-  static const Color black = Color(0xFF20394f); // 黒色
+  static const Color black = Color(0xff221967); // 黒色
 }
