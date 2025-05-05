3a352218d90cc2fbc1197676d335764f397618fe
KanekoKKK <231205066@ccmailg.meijo-u.ac.jp>
2025-05-05 18:26:04 +0900

    [update] #123 fb詳細UI調整

--- lib/widget_fbsheet.dart
+++ lib/widget_fbsheet.dart
@@ -1,201 +1,250 @@
 import 'package:flutter/material.dart';
 import 'package:ps_hacku_osaka/colors.dart';
 
-class FbSheet extends StatefulWidget {  
+class FbSheet extends StatefulWidget {
   final List<String> labels; //ラベル
-  final String problem;      //問題文
-  final String wrong;        //間違えてた部分
+  final String problem; //問題文
+  final String wrong; //間違えてた部分
   final String wrongpartans; //間違えてた部分の正しい解き方
-  final String correctans;   //全体の正しい解き方
+  final String correctans; //全体の正しい解き方
 
   const FbSheet({
     super.key,
     this.labels = const [],
-    this.problem = "none",
-    this.wrong = "none",
-    this.wrongpartans = "none",
-    this.correctans = "none",
-    });
+    this.problem = "------------------------------",
+    this.wrong = "------------------------------",
+    this.wrongpartans = "------------------------------",
+    this.correctans = "------------------------------",
+  });
 
   @override
   State<FbSheet> createState() => _FbSheetState();
 }
 
 class _FbSheetState extends State<FbSheet> {
-  // フィードバック1
-  String fbTitle1 = "間違えてたとこ"; //　間違えてた部分
-  String fbText1 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-  // フィードバック2
-  String fbTitle2 = "ピンポイント解説"; //　間違えてた部分の正しい解き方
-  String fbText2 = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb";
-  // フィードバック3
-  String fbTitle3 = "全体の回答"; // 全体の正しい解き方
-  String fbText3 = "cccccccccccccccccccccccccccccccc";
-  // 内容が空だった場合のテキスト
-  String fbErrorText = "。。。";
+  final ScrollController _scrollController = ScrollController(); // スクロールのコントローラ
 
   @override
-  void initState() {
-    super.initState();
-    fbText1 = widget.wrong; //仮設定
-    fbText2 = widget.wrongpartans;
-    fbText3 = widget.correctans;
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(vertical: 16),
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: A_Colors.white,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        children: [
-
-          // ▼ ---------- フィードバック1 ---------- ▼ //
-          Container(
-            width: MediaQuery.of(context).size.width * 0.9,
-            alignment: Alignment.centerLeft,
-            margin: const EdgeInsets.only(top: 8, bottom: 16),
-            padding: const EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              color: A_Colors.subColor,
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Column(
-              children: [
-                // タイトル
-                Center(
-                  child: Text(
-                    fbTitle1,
-                    style: TextStyle(
-                        color: A_Colors.black,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  ),
+        decoration: BoxDecoration(
+          color: A_Colors.white,
+        ),
+        child: Column(
+          children: [
+            // ▼ ---------- 問題文(problem) ---------- ▼ //
+            Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [A_Colors.mainColor, A_Colors.black],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
                 ),
+              ),
+              child: Text(
+                widget.problem,
+                style: TextStyle(
+                  color: A_Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
 
-                // テキスト枠
+            // ▲ ---------- 問題文(problem) ---------- ▲ //
+            Expanded(
+                child: SingleChildScrollView(
+              controller: _scrollController,
+              child: Column(children: [
+                // ▼ ---------- 間違えた部分(wrong) ---------- ▼ //
                 Container(
                   width: MediaQuery.of(context).size.width * 0.9,
                   alignment: Alignment.centerLeft,
-                  margin: const EdgeInsets.only(top: 8, bottom: 16),
+                  margin: const EdgeInsets.only(top: 8, right: 8, left: 8),
                   padding: const EdgeInsets.all(16),
                   decoration: BoxDecoration(
-                    color: A_Colors.white,
+                    gradient: LinearGradient(
+                      colors: [A_Colors.subColor, A_Colors.background],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
                     borderRadius: BorderRadius.circular(16),
                   ),
-                  // テキスト文章
-                  child: Text(
-                    fbText1,
-                    style: TextStyle(
-                      color: A_Colors.black,
-                      fontSize: 16,
-                    ),
+                  child: Column(
+                    children: [
+                      // タイトル
+                      Center(
+                        child: Text(
+                          'つまづきポイント',
+                          style: TextStyle(
+                            color: A_Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            shadows: [
+                              Shadow(
+                                blurRadius: 20,
+                                color: A_Colors.black,
+                                offset: Offset(0, 0),
+                                
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+
+                      // テキスト枠
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        alignment: Alignment.centerLeft,
+                        margin: const EdgeInsets.only(top: 4),
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: A_Colors.white,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        // テキスト文章
+                        child: Text(
+                          widget.wrong,
+                          style: TextStyle(
+                            color: A_Colors.black,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
-          // ▲ ---------- フィードバック1 ---------- ▲ //
+                // ▲ ---------- 間違えた部分(wrong) ---------- ▲ //
 
-          // ▼ ---------- フィードバック2 ---------- ▼ //
-          Container(
-            width: MediaQuery.of(context).size.width * 0.9,
-            alignment: Alignment.centerLeft,
-            margin: const EdgeInsets.only(top: 8, bottom: 16),
-            padding: const EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              color: A_Colors.subColor,
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Column(
-              children: [
-                // タイトル
-                Center(
-                  child: Text(
-                    fbTitle2,
-                    style: TextStyle(
-                        color: A_Colors.black,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-
-                // テキスト枠
+                // ▼ ---------- 間違えた部分の正しい解き方(wrongpartans) ---------- ▼ //
                 Container(
                   width: MediaQuery.of(context).size.width * 0.9,
                   alignment: Alignment.centerLeft,
-                  margin: const EdgeInsets.only(top: 8, bottom: 16),
+                  margin: const EdgeInsets.only(top: 8, right: 8, left: 8),
                   padding: const EdgeInsets.all(16),
                   decoration: BoxDecoration(
-                    color: A_Colors.white,
+                    gradient: LinearGradient(
+                      colors: [A_Colors.subColor, A_Colors.background],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
                     borderRadius: BorderRadius.circular(16),
                   ),
-                  // テキスト文章
-                  child: Text(
-                    fbText2,
-                    style: TextStyle(
-                      color: A_Colors.black,
-                      fontSize: 16,
-                    ),
+                  child: Column(
+                    children: [
+                      // タイトル
+                      Center(
+                        child: Text(
+                          'ピンポイント解説',
+                          style: TextStyle(
+                            color: A_Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            shadows: [
+                              Shadow(
+                                blurRadius: 20,
+                                color: A_Colors.black,
+                                offset: Offset(0, 0),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+
+                      // テキスト枠
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        alignment: Alignment.centerLeft,
+                        margin: const EdgeInsets.only(top: 4),
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: A_Colors.white,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        // テキスト文章
+                        child: Text(
+                          widget.wrongpartans,
+                          style: TextStyle(
+                            color: A_Colors.black,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
-          // ▲ ---------- フィードバック2 ---------- ▲ //
+                // ▲ ---------- 間違えた部分の正しい解き方(wrongpartans) ---------- ▲ //
 
-          // ▼ ---------- フィードバック3 ---------- ▼ //
-          Container(
-            width: MediaQuery.of(context).size.width * 0.9,
-            alignment: Alignment.centerLeft,
-            margin: const EdgeInsets.only(top: 8, bottom: 16),
-            padding: const EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              color: A_Colors.subColor,
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Column(
-              children: [
-                // タイトル
-                Center(
-                  child: Text(
-                    fbTitle3,
-                    style: TextStyle(
-                        color: A_Colors.black,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-
-                // テキスト枠
+                // ▼ ---------- 全体の正しい解き方(correctans) ---------- ▼ //
                 Container(
                   width: MediaQuery.of(context).size.width * 0.9,
                   alignment: Alignment.centerLeft,
-                  margin: const EdgeInsets.only(top: 8, bottom: 16),
+                  margin: const EdgeInsets.only(top: 8, right: 8, left: 8),
                   padding: const EdgeInsets.all(16),
                   decoration: BoxDecoration(
-                    color: A_Colors.white,
+                    gradient: LinearGradient(
+                      colors: [A_Colors.subColor, A_Colors.background],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
                     borderRadius: BorderRadius.circular(16),
                   ),
-                  // テキスト文章
-                  child: Text(
-                    fbText3,
-                    style: TextStyle(
-                      color: A_Colors.black,
-                      fontSize: 16,
-                    ),
+                  child: Column(
+                    children: [
+                      // タイトル
+                      Center(
+                        child: Text(
+                          '全体の解説',
+                          style: TextStyle(
+                            color: A_Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            shadows: [
+                              Shadow(
+                                blurRadius: 20,
+                                color: A_Colors.black,
+                                offset: Offset(0, 0),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+
+                      // テキスト枠
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        alignment: Alignment.centerLeft,
+                        margin: const EdgeInsets.only(top: 4),
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: A_Colors.white,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        // テキスト文章
+                        child: Text(
+                          widget.wrong,
+                          style: TextStyle(
+                            color: A_Colors.black,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
-          // ▲ ---------- フィードバック3 ---------- ▲ //
-        ],
-      )
-    );
+                // ▲ ---------- 全体の正しい解き方(correctans) ---------- ▲ //
+              ]),
+            ))
+          ],
+        ));
   }
 }
